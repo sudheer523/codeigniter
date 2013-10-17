d99fac19a3cadb70245865c0b4985abedcae6135
sudheer523 <sudheer.kavitapu@nuncsystems.com>
2013-10-17 13:12:41 +0530

    Codeigniter-Webframework
    It is a webframework

---
+++ codeigniter/application/models/user_model.php
@@ -0,0 +1,18 @@
+<?php
+Class User_model extends CI_Model
+{
+ function __construct()
+  {
+  
+   parent::__construct();
+  }
+ function register_user($username,$name,$email,$password)  
+   {
+     $sha1_password=sha1($password);
+     $query="insert into user (username,name,email,password) values(?,?,?,?)";
+	 $this->db->query($query,array($username,$name,$email,$sha1_password));
+	 
+   }
+
+}
+?>
